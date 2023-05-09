09623cfe9d28f2de3d16bc5c7709b44ed98f2ddf
Razvan Bratu <braturazvan81@yahoo.ro>
2023-05-09 14:18:33 +0300

    Basics of Dependency Injection

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -1,13 +1,21 @@
 package com.example.practice;
 
+import com.example.practice.controllers.MyController;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(PracticeApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
