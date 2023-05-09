27a81e525d632f68b87e8c1a4349a6fff375909d
Razvan Bratu <braturazvan81@yahoo.ro>
2023-05-09 19:23:43 +0300

    Spring Profiles

--- practice/src/main/java/com/example/practice/services/I18nSpanishGreetingService.java
+++ practice/src/main/java/com/example/practice/services/I18nSpanishGreetingService.java
@@ -2,7 +2,7 @@ package com.example.practice.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ESP")
+@Profile({"ESP", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
